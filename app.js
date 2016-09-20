a633a8c4548ffad399a967ae77478a66f83b919a
Antoine Lenoir <antoine.alenoir@gmail.com>
2016-09-20 12:21:57 +0200

    get access token

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var session = require('express-session')
+var passport = require('passport');
+var TwitterStrategy = require('passport-twitter');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -6,9 +9,45 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
+
+app.use(session({ secret: 'SECRET' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function (req, res, next) {
+  var consumer_key = req.session.consumer_key || req.query.consumer_key;
+  var consumer_secret = req.session.consumer_secret || req.query.consumer_secret;
+
+  if (consumer_key && consumer_secret) {
+    req.session.consumer_key = consumer_key;
+    req.session.consumer_secret = consumer_secret;
+
+    passport.use(new TwitterStrategy(
+      {
+        consumerKey: consumer_key,
+        consumerSecret: consumer_secret,
+        callbackURL: "http://127.0.0.1:4000/auth/twitter/callback"
+      },
+      function(token, tokenSecret, profile, cb) {
+        profile.token = token;
+        profile.tokenSecret = tokenSecret;
+
+        cb(null, profile)
+      }
+    ));
+  }
+  next();
+});
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +62,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
+var passport = require('passport');
+var TwitterStrategy = require('passport-twitter');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/token', function(req, res, next) {
+  if (req.user &&  req.user.token &&  req.user.tokenSecret) {
+    var token = req.user.token;
+    var tokenSecret = req.user.tokenSecret;
+    res.render('token', {
+      token: token,
+      tokenSecret: tokenSecret
+    });
+  } else {
+    res.redirect('/');
+  }
+  req.session.destroy();
+  req.logout();
 });
+
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/auth/twitter', passport.authenticate('twitter'));
+
+router.get('/auth/twitter/callback',
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/token');
+  });
 
 module.exports = router;
